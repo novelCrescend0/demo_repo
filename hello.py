5a7edbeb93bb4b1cbd32e8c393dd4a911a86210c
novelCrescend0 <168645232+novelCrescend0@users.noreply.github.com>
2024-05-01 18:10:14 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,25 +1,7 @@
-<?xml version="1.0" encoding="utf-8"?>
-<resources>
-    <!-- Yellow -->
-    <color name="bright_yellow">#FFFF00</color>
-
-    <!-- Orange -->
-    <color name="vibrant_orange">#FFA500</color>
-
-    <!-- Sky Blue -->
-    <color name="clear_sky_blue">#87CEEB</color>
-
-    <!-- Green -->
-    <color name="fresh_green">#00FF00</color>
-
-    <!-- Pink -->
-    <color name="soft_pink">#FFC0CB</color>
-
-    <!-- Lavender -->
-    <color name="soothing_lavender">#E6E6FA</color>
-
-    <!-- Pastel Colors -->
-    <color name="pastel_mint_green">#98FB98</color>
-    <color name="pastel_baby_blue">#ADD8E6</color>
-    <color name="pastel_pale_yellow">#FFFFE0</color>
-</resources>
+<ImageView
+    android:id="@+id/imageView"
+    android:layout_width="wrap_content"
+    android:layout_height="wrap_content"
+    android:src="@drawable/your_image"
+    android:layout_centerInParent="true"
+    android:animation="@anim/move_animation"/>
