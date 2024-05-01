3eb11a01ef366dde8f48e1cfe0d9374a1007875d
novelCrescend0 <168645232+novelCrescend0@users.noreply.github.com>
2024-05-01 18:14:01 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,5 +3,8 @@
     android:layout_width="wrap_content"
     android:layout_height="wrap_content"
     android:src="@drawable/your_image"
-    android:layout_centerInParent="true"
-    android:animation="@anim/move_animation"/>
+    app:layout_constraintStart_toStartOf="parent"
+    app:layout_constraintTop_toTopOf="parent"
+    app:layout_constraintEnd_toEndOf="parent"
+    app:layout_constraintBottom_toBottomOf="parent"/>
+
